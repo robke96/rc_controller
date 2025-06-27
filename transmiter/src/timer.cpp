62151d8149e778ccc8a06c3f9be89def92784c87
robke96 <robertaszencius@gmail.com>
2025-06-27 10:02:09 +0300

    timer device turn off

--- transmiter/src/timer.cpp
+++ transmiter/src/timer.cpp
@@ -5,6 +5,15 @@ unsigned long timerStart = 0;
 const unsigned long timerDuration = 180000; //3min
 bool timerRunning = false;
 
+void deviceTurningOff() {
+    Serial.println("Turning off arduino in 3s");
+    digitalWrite(LED_WHITE_PIN, LOW);
+    delay(3000);
+
+    // kill power
+    digitalWrite(POWER_PIN, LOW);
+};
+
 void checkTimer(bool activity) {
     if (!timerRunning) {
         timerStart = millis();
@@ -16,12 +25,11 @@ void checkTimer(bool activity) {
         timerStart = millis();
     }
 
+    // no interaction == device turn off
     if (millis() - timerStart >= timerDuration) {
         timerRunning = false;
-        Serial.println("Turning off arduino in 3s");
-        digitalWrite(LED_WHITE_PIN, LOW);
-        delay(3000);
-        digitalWrite(POWER_PIN, LOW);
+
+        deviceTurningOff();
     }
   
 }
